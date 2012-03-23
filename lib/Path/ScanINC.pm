7ab6a059b7c97fff58954a2323d2f6f1f2f30f27
Kent Fredric <kentfredric@gmail.com>
2012-03-24 06:33:25 +1300

    Build results of 82fe7a7 (on master)

--- lib/Path/ScanINC.pm
+++ lib/Path/ScanINC.pm
@@ -2,80 +2,15 @@ use strict;
 use warnings;
 
 package Path::ScanINC;
+BEGIN {
+  $Path::ScanINC::AUTHORITY = 'cpan:KENTNL';
+}
+{
+  $Path::ScanINC::VERSION = '0.001';
+}
 
 # ABSTRACT: Emulate Perls internal handling of @INC.
 
-=head1 SYNOPSIS
-
-The Aim of this module is to fully implement everything Perl does with C<@INC>, to be feature compatible with it, including
-the behaviour with regard to C<sub refs> in C<@INC>.
-
-
-	use Path::ScanINC;
-
-	# Normal usage.
-	my $inc = Path::ScanINC->new( );
-
-	# In case you need something that isn't @INC
-	# but works like it
-
-	my $inc = Path::ScanINC->new( inc => \@INC );
-
-	# Freeze the value of @INC at the time of object instantiation
-	# with regard to behaviour so later changes to @INC have no effect
-
-	my $inc = Path::ScanINC->new( immutable => 1 );
-
-	# Return the first file in @INC that matches.
-
-	my $file = $inc->first_file('Path', 'ScanINC.pm' );
-
-	# Find all possible versions of modules in @INC
-	my ( @files ) = $inc->all_files('Path', 'ScanINC.pm');
-
-	# Try to discover a File::ShareDir 'module' root.
-	my $dir = $inc->first_dir('auto','share','module');
-
-	# Should return the same as File::ShareDir::module_dir('Path::ScanINC')
-	# ( assuming such a directory existed, which there is presently no plans of )
-	my $dir = $inc->first_dir('auto','share','module','Path-ScanINC');
-
-
-	# Find All File::ShareDir roots in @INC
-	my ( @dirs ) = $inc->all_dirs('auto', 'share');
-
-=head1 REF SUPPORT IN @INC
-
-This module has elemental support for discovery of results in C<@INC> using C<CODE>/C<ARRAY>/C<BLESSED> entries in
-C<@INC>. However, due to a limitation as to how perl itself implements this functionality, the best we can do at present
-is simply return what the above are expected to return. This means if you have any of the above ref-types in C<@INC>,
-and one of those returns C<a true value>, you'll get handed back an C<ARRAY> reference instead of the file you were
-expecting.
-
-Fortunately, C<@INC> barely ever has refs in it. But in the event you I<need> to work with refs in C<@INC> and you
-expect that those refs will return C<true>, you have to pick one of two options, either :
-
-=over 4
-
-=item a. Write your code to work with the C<array-ref> returned by the respective reference on a match
-
-=item b. Use the C<all_> family of methods and try pretendeding that there are no C<array-refs> in the list it returns.
-
-=back
-
-Its possible in a future release we may have better choices how to handle this situation in future, but don't bet on it.
-
-Given that the API as defined by Perl mandates C<code-ref>'s return lists containing C<file-handles> or iterative
-C<code-ref>'s , not actual files, the best I can forsee at this time we'd be able to do to make life easier for you is
-creating a fake library somewhere in a C<tempdir> and stuffing the result of the C<code-ref>'s into files in that dir
-prior to returning a path to the generated file.
-
-( And it also tells me that they have to be "Real" file handles, not tied or blessed ones, so being able to ask a
-filehandle what file it represents is equally slim.... if that is of course what you require )
-
-For more details, see L<< C<perldoc perlfunc> or C<perldoc -f require> |perlfunc/require >>.
-
-=cut
 
 # Sub Lazy-Aliases
 
@@ -147,14 +82,6 @@ sub __check_object_method {
 	return 1;
 }
 
-=method new
-
-	my $object = $class->new(
-		 inc => [ 'x', 'y', 'z' , ],
-		 immutable => 1 | undef
-	);
-
-=cut
 
 sub new {
 	my ( $class, @args ) = @_;
@@ -195,13 +122,6 @@ sub _new {
 	return $obj;
 }
 
-=method immutable
-
-	if( $inc->immutable ) {
-		print "We're working with a snapshotted version of @INC";
-	}
-
-=cut
 
 sub immutable {
 	my ( $obj, @args ) = @_;
@@ -234,31 +154,6 @@ sub _init_immutable {
 	return $obj;
 }
 
-=method inc
-
-	for my $i ( $inc->inc ) {
-		say "Plain: $incer" if not ref $incer;
-		say "Callback: $incer" if ref $incer;
-	}
-
-Returns a copy of the internal version of C<@INC> it will be using.
-
-If the object is C<immutable>, then this method will continue to report the same value as c<@INC>, or will be updated
-every time the orignal array reference passed during construction gets updated:
-
-	my $ref = [];
-	my $a = Path::ScanINC->new( inc => $inc );
-	my $b = Path::ScanINC->new( inc => $inc, immutable => 1 );
-
-	push @{$ref} , 'a';
-
-	is( [ $a->inc ]->[0] , 'a' , "non-immutable references keep tracking their original" );
-	isnt( [ $a->inc ]->[0] , 'a' , "immutable references are shallow-copied at construction" );
-
-Do note of course that is a B<SHALLOW> copy, so if you have multiple C<@INC> copies sharing the same C<array>/C<bless>
-references, changes to those references will be shared amongst all C<@INC>'s .
-
-=cut
 
 sub inc {
 	my ( $obj, @args ) = @_;
@@ -328,21 +223,6 @@ sub _ref_expand {
 	return [ undef, ];
 }
 
-=method first_file
-
-	if( defined ( my $file = $inc->first_file('Moose.pm') ) {
-		print "Yep, Moose seems to be available in \@INC , its at $file, but its not loaded (yet)\n";
-	}
-
-This proves to be a handy little gem that replaces the oft used
-
-	if( try { require Moose ; 1 } ){
-		Yadayadayada
-	}
-
-And adds the benefit of not needing to actually source the file to see if it exists or not.
-
-=cut
 
 sub first_file {
 	my ( $self, @args ) = @_;
@@ -368,7 +248,216 @@ sub first_file {
 	return;
 }
 
-=method all_files
+
+sub all_files {
+	my ( $self, @args ) = @_;
+	__check_object_method( $self, 'all_files' );
+	require File::Spec;
+	my $suffix = File::Spec->catfile(@args);
+	my @out;
+	for my $path ( $self->inc ) {
+		if ( ref $path ) {
+			my $result = $self->_ref_expand( $path, $suffix );
+			if ( $result->[0] ) {
+				shift @{$result};
+				push @out, $result;
+			}
+			next;
+		}
+		require File::Spec;
+		my $fullpath = File::Spec->catfile( $path, $suffix );
+		if ( -e $fullpath and -f $fullpath ) {
+			push @out, $fullpath;
+		}
+	}
+	return @out;
+}
+
+
+sub first_dir {
+	my ( $self, @args ) = @_;
+	__check_object_method( $self, 'first_dir' );
+	require File::Spec;
+	my $suffix = File::Spec->catdir(@args);
+	for my $path ( $self->inc ) {
+		if ( ref $path ) {
+			my $result = $self->_ref_expand( $path, $suffix );
+			if ( $result->[0] ) {
+				shift @{$result};
+				return $result;
+			}
+			next;
+		}
+		my $fullpath = File::Spec->catdir( $path, $suffix );
+		if ( -e $fullpath and -d $fullpath ) {
+			return $fullpath;
+		}
+	}
+	return;
+}
+
+
+sub all_dirs {
+	my ( $self, @args ) = @_;
+	__check_object_method( $self, 'all_dirs' );
+	require File::Spec;
+	my $suffix = File::Spec->catdir(@args);
+	my @out;
+	for my $path ( $self->inc ) {
+		if ( ref $path ) {
+			my $result = $self->_ref_expand( $path, $suffix );
+			if ( $result->[0] ) {
+				shift @{$result};
+				push @out, $result;
+			}
+			next;
+		}
+		my $fullpath = File::Spec->catdir( $path, $suffix );
+		if ( -e $fullpath and -d $fullpath ) {
+			push @out, $fullpath;
+		}
+	}
+	return @out;
+}
+
+1;
+
+__END__
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Path::ScanINC - Emulate Perls internal handling of @INC.
+
+=head1 VERSION
+
+version 0.001
+
+=head1 SYNOPSIS
+
+The Aim of this module is to fully implement everything Perl does with C<@INC>, to be feature compatible with it, including
+the behaviour with regard to C<sub refs> in C<@INC>.
+
+	use Path::ScanINC;
+
+	# Normal usage.
+	my $inc = Path::ScanINC->new( );
+
+	# In case you need something that isn't @INC
+	# but works like it
+
+	my $inc = Path::ScanINC->new( inc => \@INC );
+
+	# Freeze the value of @INC at the time of object instantiation
+	# with regard to behaviour so later changes to @INC have no effect
+
+	my $inc = Path::ScanINC->new( immutable => 1 );
+
+	# Return the first file in @INC that matches.
+
+	my $file = $inc->first_file('Path', 'ScanINC.pm' );
+
+	# Find all possible versions of modules in @INC
+	my ( @files ) = $inc->all_files('Path', 'ScanINC.pm');
+
+	# Try to discover a File::ShareDir 'module' root.
+	my $dir = $inc->first_dir('auto','share','module');
+
+	# Should return the same as File::ShareDir::module_dir('Path::ScanINC')
+	# ( assuming such a directory existed, which there is presently no plans of )
+	my $dir = $inc->first_dir('auto','share','module','Path-ScanINC');
+
+
+	# Find All File::ShareDir roots in @INC
+	my ( @dirs ) = $inc->all_dirs('auto', 'share');
+
+=head1 REF SUPPORT IN @INC
+
+This module has elemental support for discovery of results in C<@INC> using C<CODE>/C<ARRAY>/C<BLESSED> entries in
+C<@INC>. However, due to a limitation as to how perl itself implements this functionality, the best we can do at present
+is simply return what the above are expected to return. This means if you have any of the above ref-types in C<@INC>,
+and one of those returns C<a true value>, you'll get handed back an C<ARRAY> reference instead of the file you were
+expecting.
+
+Fortunately, C<@INC> barely ever has refs in it. But in the event you I<need> to work with refs in C<@INC> and you
+expect that those refs will return C<true>, you have to pick one of two options, either :
+
+=over 4
+
+=item a. Write your code to work with the C<array-ref> returned by the respective reference on a match
+
+=item b. Use the C<all_> family of methods and try pretendeding that there are no C<array-refs> in the list it returns.
+
+=back
+
+Its possible in a future release we may have better choices how to handle this situation in future, but don't bet on it.
+
+Given that the API as defined by Perl mandates C<code-ref>'s return lists containing C<file-handles> or iterative
+C<code-ref>'s , not actual files, the best I can forsee at this time we'd be able to do to make life easier for you is
+creating a fake library somewhere in a C<tempdir> and stuffing the result of the C<code-ref>'s into files in that dir
+prior to returning a path to the generated file.
+
+( And it also tells me that they have to be "Real" file handles, not tied or blessed ones, so being able to ask a
+filehandle what file it represents is equally slim.... if that is of course what you require )
+
+For more details, see L<< C<perldoc perlfunc> or C<perldoc -f require> |perlfunc/require >>.
+
+=head1 METHODS
+
+=head2 new
+
+	my $object = $class->new(
+		 inc => [ 'x', 'y', 'z' , ],
+		 immutable => 1 | undef
+	);
+
+=head2 immutable
+
+	if( $inc->immutable ) {
+		print "We're working with a snapshotted version of @INC";
+	}
+
+=head2 inc
+
+	for my $i ( $inc->inc ) {
+		say "Plain: $incer" if not ref $incer;
+		say "Callback: $incer" if ref $incer;
+	}
+
+Returns a copy of the internal version of C<@INC> it will be using.
+
+If the object is C<immutable>, then this method will continue to report the same value as c<@INC>, or will be updated
+every time the orignal array reference passed during construction gets updated:
+
+	my $ref = [];
+	my $a = Path::ScanINC->new( inc => $inc );
+	my $b = Path::ScanINC->new( inc => $inc, immutable => 1 );
+
+	push @{$ref} , 'a';
+
+	is( [ $a->inc ]->[0] , 'a' , "non-immutable references keep tracking their original" );
+	isnt( [ $a->inc ]->[0] , 'a' , "immutable references are shallow-copied at construction" );
+
+Do note of course that is a B<SHALLOW> copy, so if you have multiple C<@INC> copies sharing the same C<array>/C<bless>
+references, changes to those references will be shared amongst all C<@INC>'s .
+
+=head2 first_file
+
+	if( defined ( my $file = $inc->first_file('Moose.pm') ) {
+		print "Yep, Moose seems to be available in \@INC , its at $file, but its not loaded (yet)\n";
+	}
+
+This proves to be a handy little gem that replaces the oft used
+
+	if( try { require Moose ; 1 } ){
+		Yadayadayada
+	}
+
+And adds the benefit of not needing to actually source the file to see if it exists or not.
+
+=head2 all_files
 
 Returns all matches in all C<@INC> paths.
 
@@ -399,87 +488,24 @@ I might even be tempted to make a sub-module to make one-liners easier like
 
 B<REMINDER>: If there are C<REFS> in C<@INC> that match, they'll return C<array-ref>'s, not strings.
 
-=cut
-
-sub all_files {
-	my ( $self, @args ) = @_;
-	__check_object_method( $self, 'all_files' );
-	require File::Spec;
-	my $suffix = File::Spec->catfile(@args);
-	my @out;
-	for my $path ( $self->inc ) {
-		if ( ref $path ) {
-			my $result = $self->_ref_expand( $path, $suffix );
-			if ( $result->[0] ) {
-				shift @{$result};
-				push @out, $result;
-			}
-			next;
-		}
-		require File::Spec;
-		my $fullpath = File::Spec->catfile( $path, $suffix );
-		if ( -e $fullpath and -f $fullpath ) {
-			push @out, $fullpath;
-		}
-	}
-	return @out;
-}
-
-=method first_dir
+=head2 first_dir
 
 Just like C<first_file> except for locating directories.
 
-=cut
-
-sub first_dir {
-	my ( $self, @args ) = @_;
-	__check_object_method( $self, 'first_dir' );
-	require File::Spec;
-	my $suffix = File::Spec->catdir(@args);
-	for my $path ( $self->inc ) {
-		if ( ref $path ) {
-			my $result = $self->_ref_expand( $path, $suffix );
-			if ( $result->[0] ) {
-				shift @{$result};
-				return $result;
-			}
-			next;
-		}
-		my $fullpath = File::Spec->catdir( $path, $suffix );
-		if ( -e $fullpath and -d $fullpath ) {
-			return $fullpath;
-		}
-	}
-	return;
-}
-
-=method all_dirs
+=head2 all_dirs
 
 Just like C<all_dirs> except for locating directories.
 
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Kent Fredric <kentnl@cpan.org>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 =cut
 
-sub all_dirs {
-	my ( $self, @args ) = @_;
-	__check_object_method( $self, 'all_dirs' );
-	require File::Spec;
-	my $suffix = File::Spec->catdir(@args);
-	my @out;
-	for my $path ( $self->inc ) {
-		if ( ref $path ) {
-			my $result = $self->_ref_expand( $path, $suffix );
-			if ( $result->[0] ) {
-				shift @{$result};
-				push @out, $result;
-			}
-			next;
-		}
-		my $fullpath = File::Spec->catdir( $path, $suffix );
-		if ( -e $fullpath and -d $fullpath ) {
-			push @out, $fullpath;
-		}
-	}
-	return @out;
-}
-
-1;
